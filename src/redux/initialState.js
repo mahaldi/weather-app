596d360402e088647d877ab5588aa77dc3470afc
mahaldi <aldiwijayasaputra@gmail.com>
2020-12-13 02:58:07 +0700

    redux initial state

---
+++ src/redux/initialState.js
@@ -0,0 +1,12 @@
+const DEFAULT = {
+	data: undefined,
+	loading: true,
+	error: undefined,
+}
+const initialState = {
+	currentLocation: DEFAULT,
+	currentWeather: DEFAULT,
+	oneCall: DEFAULT
+}
+
+export default initialState

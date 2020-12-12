113f4b950abccd2b16c6707064fac7d436f18812
mahaldi <aldiwijayasaputra@gmail.com>
2020-12-13 03:00:14 +0700

    import js-cookie

--- src/api/oneCall.js
+++ src/api/oneCall.js
@@ -1,4 +1,6 @@
 import api from './index'
+import Cookie from 'js-cookie'
+
 const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY
 const langCookieName = process.env.REACT_APP_LANG_COOKIE_NAME
 const CURRENT_PREFFERED_LANG = Cookie.get(langCookieName) || process.env.REACT_APP_DEFAULT_PREFERRED_LANG

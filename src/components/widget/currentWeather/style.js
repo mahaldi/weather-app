3a0b89ae2d21ae70d25bf2ea3979f5549439996e
mahaldi <aldiwijayasaputra@gmail.com>
2020-12-13 15:09:50 +0700

    component current weather

---
+++ src/components/widget/currentWeather/style.js
@@ -0,0 +1,14 @@
+import {makeStyles} from '@material-ui/styles'
+
+const useStyle = makeStyles((theme)=> ({
+	root: {
+		// backgroundImage: 'linear-gradient(#1a357c,#5a3e8c)',
+		borderRadius: theme.shape.borderRadius,
+		padding: theme.spacing(2),
+		boxShadow: '0 0 12px 0 rgba(0,0,0,.2)',
+		backgroundColor: 'white',
+		maxHeight: 331
+	}
+}))
+
+export default useStyle
